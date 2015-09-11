a28862fb6f9c51e32a94145fea42472816bf9d61
tigerrlao <tigerrlao@gmail.com>
2015-09-11 07:48:22 -0700

    Dice

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,11 +1,27 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die one;
-void setup()
+public void setup()
 {
 	noLoop();
 	size(600,600);
 	
 }
-void draw()
+public void draw()
 {
 	strokeWeight(10);
 	background(127);
@@ -18,7 +34,7 @@ void draw()
 		}
 	}
 }
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -26,13 +42,14 @@ class Die //models one single dice cube
 {
 	int myX, myY;
 	int rolling = (int)(Math.random()*6)+1;
-	Die(int x,int y)
+	Die(int x,int y)    //constructor
 	{
 		myX = x;
 		myY = y;
 	}
-	void show()
+	public void show()
 	{
+		System.out.println(rolling);
 		fill(255);
 		rect(myX,myY,200,200);
 		fill(0);
@@ -68,4 +85,13 @@ class Die //models one single dice cube
 			}
 		}
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
